961e73ae4513e959193f5a696f33927e8cdbdc86
liuyaochi <445901885@qq.com>
2020-09-19 18:35:30 +0800

    '初始化项目'

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<style>
+
+
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from  'axios'  //引入别忘了！！！
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+axios({
+  url: 'http://123.207.32.32:8000/home/multidata',
+  //默认返回的是一个Promise
+}).then( res => {
+  console.log(res);
+})
